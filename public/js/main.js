9cd15401509c3cd609193a785cc3b95b24bcff66
PKPurdue <pkrivack@purdue.edu>
2019-01-10 22:39:15 -0500

    works
    working version

---
+++ public/js/main.js
@@ -0,0 +1,213 @@
+var socket = io();
+var socket_id;
+var groups = undefined;
+var userToken = "";
+var lastFinishClick = 0;
+
+socket.on('userTokenResponse', function(data)
+{
+	console.log(JSON.stringify(data));
+	groups = data;
+	var groupsSelection = $("#userGroups")[0];
+	for (var i = 0; i < groups.length; i++)
+	{
+		var group = groups[i];
+		
+		var clon = groupsSelection.children[0].cloneNode(true);
+		clon.style.display = "block";
+		clon.value = (group.ClassName == "DM" && group.UserId || group.GroupId);
+		clon.setAttribute("ClassName", group.ClassName);
+		clon.innerHTML = (group.ClassName == "DM" && "(DM) " || "") + group.Name;
+		groupsSelection.appendChild(clon);
+	}
+	$("#accessTokenFrame")[0].style.display = "none";
+	$("#groupFrame")[0].style.display = "block";
+});
+
+socket.on('spamMessageGroupResponse', function(data)
+{
+	alert("Success! " + data);
+	window.location.reload();
+});
+
+socket.on('kickUsersInGroupResponse', function(data)
+{
+	alert("Success! " + data);
+	window.location.reload();
+});
+
+function tokenInputted()
+{
+	var tokenInput = $("#tokenInput")[0];
+	var tokenButton = $("#tokenButton")[0];
+	if (tokenInput.value.length > 15)
+	{
+		tokenButton.style.display = "block";
+	}
+	else
+	{
+		tokenButton.style.display = "none";
+	}
+}
+
+function submitToken()
+{
+	var tokenInput = $("#tokenInput")[0];
+	userToken = tokenInput.value;
+	socket.emit('userToken', {token: tokenInput.value});
+}
+
+function commandSelected()
+{
+	$("#groupSelectDiv")[0].style.display = "";
+	
+	$("#userSelectDiv")[0].style.display = "none";
+	$("#messageDiv")[0].style.display = "none";
+	$("#messageCountDiv")[0].style.display = "none";
+	$("#submitButton")[0].style.display = "none";
+	
+	var groupElement = $("#userGroups")[0];
+	var groupName = groupElement[groupElement.selectedIndex].innerHTML;
+	
+	if (groupName != "Choose a Group") { groupSelected(); }
+}
+
+function addMemberElements()
+{
+	var userSelect = $("#selectUsers")[0];
+	for (var i = userSelect.children.length - 1; i > 1; i--)
+	{
+		userSelect.children[i].remove();
+	}
+	var group = undefined;
+	var groupElement = $("#userGroups")[0];
+	var groupName = groupElement[groupElement.selectedIndex].innerHTML;
+	
+	for (var i = 0; i < groups.length; i++)
+	{
+		if (groups[i].Name == groupName)
+		{
+			group = groups[i];
+			//console.log(group);
+			break;
+		}
+	}
+	
+	var groupMembers = group.Members;
+	for (var i = 0; i < groupMembers.length; i++)
+	{
+		var clon = userSelect.children[0].cloneNode(true);
+		clon.value = groupMembers[i].UserId;
+		clon.innerHTML = groupMembers[i].Name;
+		clon.style.display = "";
+		userSelect.appendChild(clon);
+	}
+}
+
+function groupSelected()
+{
+	var commandElement = $("#commandInputSelect")[0];
+	var command = commandElement[commandElement.selectedIndex].innerHTML;
+	var userSelect = $("#userSelectDiv")[0];
+	var messageChoose = $("#messageDiv")[0];
+	var messageCount = $("#messageCountDiv")[0];
+	$("#submitButton")[0].style.display = "";
+	
+	if (command == "Spam Group")
+	{
+		userSelect.style.display = "none";
+		messageChoose.style.display = "";
+		messageCount.style.display = "";
+	}
+	else if (command == "Kick User(s) in Group")
+	{
+		addMemberElements();
+		userSelect.style.display = "";
+		messageChoose.style.display = "none";
+		messageCount.style.display = "none";
+	}
+	else
+	{
+		console.log(command);
+	}
+}
+
+function sanitizeNumber()
+{
+	var numberInput = $("#spamMessageCount")[0];
+	var parsed = parseInt(numberInput.value);
+	if (parsed == undefined || parsed < 1) { parsed = 1; }
+	if (parsed > numberInput.max) { parsed = Math.floor(parsed / 10); }
+	if (parsed == undefined || parsed < 1) { parsed = 1; }
+	
+	numberInput.value = parsed;
+}
+
+function finish()
+{
+	if ((new Date().getTime() - lastFinishClick) < 10000)
+	{
+		return;
+	}
+	lastFinishClick = new Date().getTime();
+	var commandElement = $("#commandInputSelect")[0];
+	var command = commandElement[commandElement.selectedIndex].innerHTML;
+	
+	if (command == "Spam Group")
+	{
+		var groupElement = $("#userGroups")[0];
+		var selectedElement = groupElement[groupElement.selectedIndex]
+		var groupName = selectedElement.innerHTML;
+		var groupId = selectedElement.value;
+		
+		var message = $("#spamMessageInput")[0].value;
+		var numTimes = parseInt($("#spamMessageCount")[0].value) || "1";
+		
+		var socketData = {};
+		socketData.Message = message;
+		socketData.NumberOfMessages = numTimes;
+		socketData.GroupId = groupId;
+		socketData.Token = userToken;
+		
+		socket.emit("spamMessageGroup", socketData);
+	}
+	else if (command == "Kick User(s) in Group")
+	{
+		var groupElement = $("#userGroups")[0];
+		var selectedElement = groupElement[groupElement.selectedIndex]
+		var groupName = selectedElement.innerHTML;
+		var groupId = selectedElement.value;
+		
+		var selected = $("#selectUsers")[0];
+		var options = selected.options;
+		var people = [];
+		var allUsers = false;
+		for (var i = 0; i < options.length; i++)
+		{
+			if (options[i].selected && options[i].innerHTML == "All Users")
+			{
+				allUsers = true;
+				break;
+			}
+		}
+		
+		for (var i = 0; i < options.length; i++)
+		{
+			if ((options[i].selected || allUsers == true) && options[i].innerHTML != "All Users")
+			{
+				var person = {};
+				person.Name = options[i].innerHTML;
+				person.UserId = options[i].value;
+				people.push(person);
+			}
+		}
+		
+		var socketData = {};
+		socketData.Token = userToken;
+		socketData.GroupId = groupId;
+		socketData.GroupName = groupName;
+		socketData.Users = people;
+		
+		socket.emit("kickUsersInGroup", socketData);
+	}
+}
---
+++ server.js
@@ -0,0 +1,236 @@
+const request = require('request');
+const express = require('express'); 
+const app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 32483;
+
+var users = [];
+/*
+	[socket] = {
+		UserToken: userToken,
+		Groups: groups
+		UserId: userIdInGroupMe
+	}
+	
+	[group] = {
+		Name: groupName,
+		Id: groupId,
+		Members: members
+	}
+	
+	[member] = {
+		Name: memberName,
+		UserId: memberId
+	}
+*/
+
+app.use(express.static('public'));
+app.get('/', function(req, res)
+{
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+server.listen(port, function()
+{
+    console.log('listening on ' + port);
+});
+
+
+var beginningLink = "https://api.groupme.com/v3";
+
+var commands = [];
+commands.getMyUserId = function(token, callback)
+{
+	request.get(beginningLink + "/users/me?token=" + token, function(err, response, data)
+	{
+		var newData = JSON.parse(JSON.parse(JSON.stringify(data)));
+		//console.log(JSON.stringify(newData));
+		callback(newData.response); //newData);
+	});
+}
+
+commands.getGroups = function(token, pageNum, callback)
+{
+	request.get(beginningLink + "/groups?token=" + token, {page: pageNum, per_page: 40}, function(err, response, data)
+	{
+		var newData = JSON.parse(JSON.parse(JSON.stringify(data)));
+		callback(newData.response);
+	});
+}
+
+commands.getDMs = function(token, callback)
+{
+	request.get(beginningLink + "/chats?token=" + token, {page: 1, per_page: 40}, function(err, response, data)
+	{
+		var newData = JSON.parse(JSON.parse(JSON.stringify(data)));
+		callback(newData.response);
+	});
+}
+
+commands.spamGroup = function(spamData, callback)
+{
+	var token = spamData.Token;
+	var message = spamData.Message;
+	var numTimes = spamData.NumberOfMessages;
+	var groupId = spamData.GroupId;
+	
+	var messageCount = 0;
+	var startTime = new Date().getTime();
+	
+	var i = 0;
+	var aa = setInterval(function()
+	{
+		var formJson = 
+		{
+			"message": 
+			{
+				"source_guid": message + Math.floor(Math.random() * 10000),
+				"text": message,
+				"attachments": []
+			}
+		}
+		var formData = JSON.stringify(formJson);
+		var contentLength = formData.length;
+		request({
+				headers: {
+					'Content-Length': contentLength,
+					'Content-Type': 'application/json'
+				},
+				uri: beginningLink + "/groups/" + groupId + "/messages?token=" + token,
+				body: formData,
+				method: 'POST'
+		}, function (err, res, body) {
+			messageCount++;
+			if (messageCount == numTimes)
+			{
+				var finishTime = new Date().getTime();
+				console.log("Finished in " + (finishTime - startTime) + " ms");
+				callback("Sent " + numTimes + " messages in " + (finishTime - startTime) + " ms");
+			}
+		});
+		i++;
+		if (i >= numTimes) { clearInterval(aa); }
+	}, 25);
+}
+
+commands.kickUsersInGroup = function(spamData, callback)
+{
+	var token = spamData.Token;
+	var groupId = spamData.GroupId;
+	var groupName = spamData.GroupName;
+	var people = spamData.Users;
+	
+	var kickLink = beginningLink + "/groups/" + groupId + "/members/";
+	var startTime = new Date().getTime();
+	var numKicked = 0;
+	for (var i = 0; i < people.length; i++)
+	{
+		request.post(kickLink + people[i].UserId + "/remove?token=" + token, function(err, response, body) 
+		{
+			numKicked++;
+			if (numKicked == people.length)
+			{
+				var finishTime = new Date().getTime();
+				var totalTime = finishTime - startTime;
+				callback("Kicked " + numKicked + " users from " + groupName + " in " + totalTime + " ms");
+			}
+		});
+	}
+}
+
+
+io.on('connection', function(socket)
+{
+	console.log('a user connected');
+	socket.id = Math.random();
+	
+	users[socket.id] = [];
+	var user = users[socket.id];
+	socket.on('disconnect', function()
+	{
+		delete[users[socket.id]];
+	});
+	
+	socket.on('userToken', function(data)
+	{
+		user.UserToken = data.token;
+		
+		commands.getMyUserId(data.token, function(resp)
+		{
+			user.UserId = resp.user_id;
+		});
+		
+		commands.getGroups(data.token, 1, function(resp)
+		{
+			if (resp != undefined && resp.length > 0)
+			{
+				if (user.groups != undefined && user.groups.length > 0)
+				{
+					for (var i = user.groups.length - 1; i > -1; i--)
+					{
+						delete[user.groups[i]];
+					}
+				}
+				var groups = [];
+				for (var i = 0; i < resp.length; i++)
+				{
+					var group = {};
+					group.ClassName = "Group";
+					group.Name = resp[i].name;
+					group.GroupId = resp[i].group_id;
+					group.Members = [];
+					
+					var groupMembers = resp[i].members;
+					//console.log(groupMembers);
+					for (var a = 0; a < groupMembers.length; a++)
+					{
+						if (groupMembers[a] != undefined && groupMembers[a].user_id != user.UserId)
+						{
+							var member = {};
+							member.Name = groupMembers[a].nickname || groupMembers[a].name || "Nil";
+							member.UserId = groupMembers[a].id;
+							group.Members.push(member);
+						}
+					}
+					groups.push(group);
+				}
+				user.groups = groups;
+				//console.log(user.groups);
+			}
+			commands.getDMs(data.token, function(resp)
+			{
+				if (resp != undefined && resp.length > 0)
+				{
+					if (user.groups == undefined) { user.groups = []; }
+					for (var i = 0; i < resp.length; i++)
+					{
+						var group = {};
+						group.ClassName = "DM";
+						group.Name = resp[i].other_user.name;
+						group.UserId = resp[i].other_user.id;
+						user.groups.push(group);
+					}
+					//console.log(user.groups);
+				}
+				socket.emit('userTokenResponse', user.groups);
+			});
+		});
+	});
+	
+	socket.on('spamMessageGroup', function(data)
+	{
+		commands.spamGroup(data, function(resp)
+		{
+			socket.emit('spamMessageGroupResponse', resp);
+		});
+	});
+	
+	socket.on('kickUsersInGroup', function(data)
+	{
+		commands.kickUsersInGroup(data, function(resp)
+		{
+			socket.emit('kickUsersInGroupResponse', resp);
+		});
+	});
+});
